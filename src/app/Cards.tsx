7cabbce5fd78ee80f4883876008321c89c7df65c
phukon <hello@rikiphukon.com>
2024-02-09 16:36:58 +0530

    feat: fix card sizing

--- src/app/Cards.tsx
+++ src/app/Cards.tsx
@@ -1,5 +1,5 @@
-'use client'
-import { motion, Variants } from "framer-motion";
+'use client';
+import { motion, Variants } from 'framer-motion';
 
 interface Props {
   emoji: string;
@@ -14,41 +14,42 @@ const cardVariants: Variants = {
     y: -20,
     rotate: 0,
     transition: {
-      type: "spring",
+      type: 'spring',
       bounce: 0.4,
-      duration: 1.0
-    }
-  }
+      duration: 1.0,
+    },
+  },
 };
 
-const hue = (h: number) => `hsl(${h}, 100%, 50%)`;
-
-function Card({ emoji}: Props) {
-
+function Card({ emoji }: Props) {
   return (
     <motion.div
       className="card-container"
       initial="offscreen"
       whileInView="onscreen"
-      viewport={{ once: true, amount: 1.0 }}
+      viewport={{ once: true, amount: 0.9 }}
     >
       <div className="splash" />
-      <motion.div className="card" variants={cardVariants}>
+      <div className=" flex flex-col md:flex-row justify-evenly w-full items-center">
+        <p>ededed</p>
+        <motion.div className="card" variants={cardVariants}>
         {emoji}
       </motion.div>
+      </div>
+
     </motion.div>
   );
 }
 
 const food: [string][] = [
-  ["Track your workTrack your workTrack your workTrack your work"],
-  ["Track your workTrack your workTrack your workTrack your work"],
-  ["Track your workTrack your workTrack your workTrack your work"],
-  ["Automatically sync your Google Doc activity, and view your daily writing progress. Don't worry, you can choose to keep this public or private on your page"],
+  ['Track your workTrack your workTrack your workTrack your work'],
+  ['Track your workTrack your workTrack your workTrack your work'],
+  ['Track your workTrack your workTrack your workTrack your work'],
+  [
+    "Automatically sync your Google Doc activity, and view your daily writing progress. Don't worry, you can choose to keep this public or private on your page",
+  ],
 ];
 
 export default function Cards() {
-  return food.map(([emoji], index) => (
-    <Card emoji={emoji} key={index} />
-  ));
+  return food.map(([emoji], index) => <Card emoji={emoji} key={index} />);
 }
