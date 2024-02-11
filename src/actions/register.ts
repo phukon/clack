51121a1119b6bccb7c9ae497d9c03a9583824d6a
phukon <hello@rikiphukon.com>
2024-02-11 18:17:55 +0530

    chore: bump nextAuth to beta version

--- src/actions/register.ts
+++ src/actions/register.ts
@@ -1,6 +1,9 @@
 'use server';
 import * as z from 'zod';
+import bcrypt from 'bcrypt';
 import { RegisterSchema } from '@/schemas';
+import { db } from '@/lib/db';
+import { getUserByEmail } from '@/data/user';
 
 export const register = async (values: z.infer<typeof RegisterSchema>) => {
   // validate again on the backend because client-side validation can be bypassed
@@ -10,5 +13,25 @@ export const register = async (values: z.infer<typeof RegisterSchema>) => {
     return { error: 'Invalid fields' };
   }
 
-  return {success: "Email sent!"}
+  const {email, password, name} = validatedFields.data
+  const hashedPassword = await bcrypt.hash(password, 10)
+
+  const existingUser = await getUserByEmail(email)
+
+  if (existingUser) {
+    return {error: 'Email already in use!'}
+  }
+
+  await db.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword
+    }
+  })
+
+  // TODO: Send verification token
+
+
+  return { success: 'Email sent!' };
 };
