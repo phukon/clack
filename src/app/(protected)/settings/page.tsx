29bb51d9ef4c48a509a959a274a12f6078fdeb33
phukon <hello@rikiphukon.com>
2024-02-15 10:27:25 +0530

    fix caching issues and setup navbar

--- src/app/(protected)/settings/page.tsx
+++ src/app/(protected)/settings/page.tsx
@@ -1,4 +1,9 @@
-import { auth, signOut } from '@/auth';
+/**
+ * This is server side action.
+ * I may use it in the future again.
+ */
+
+/* import { auth, signOut } from '@/auth';
 
 const SettingsPage = async () => {
   const session = await auth();
@@ -15,6 +20,31 @@ const SettingsPage = async () => {
       >
         <button type="submit">Sign out</button>
       </form>
+    </div>
+  );
+};
+
+export default SettingsPage;
+*/
+
+// -------------------------------------
+
+/**
+ * This is a combination of server action and a client component!
+ */
+'use client';
+import { logout } from '@/actions/logout';
+import { useCurrentUser } from '@/hooks/use-current-user';
+
+const SettingsPage = () => {
+  const user = useCurrentUser();
+  const onClick = () => {
+    logout();
+  };
+
+  return (
+    <div className=' bg-white p-10 rounded-xl'>
+      <button onClick={onClick}>Sign out</button>
     </div>
   );
 };
--- src/lib/tokens.ts
+++ src/lib/tokens.ts
@@ -7,7 +7,7 @@ import { getPasswordResetTokenByEmail } from '@/data/password-reset-token';
 
 export const generateTwoFactorToken = async (email: string) => {
   const token = crypto.randomInt(100_000, 1_000_000).toString()
-  const expires = new Date(new Date().getTime() + 3600 * 1000); // 1 hour wrapped in a new Date
+  const expires = new Date(new Date().getTime() + 5 * 60 * 1000); // 5 minutes
 
   const existingToken = await getTwoFactorTokenByEmail(email)
   if (existingToken) {
