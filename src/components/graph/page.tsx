1103fb2239ab56318afcb640e426c3423fafb53c
phukon <hello@rikiphukon.com>
2024-03-09 02:24:09 +0530

    transformer draft

--- src/components/graph/page.tsx
+++ src/components/graph/page.tsx
@@ -1,34 +1,37 @@
-'use client';
-import { useEffect, useRef, useState } from 'react';
-import { drawContributions } from '@/lib/graph';
-import { getUserData } from './_getData';
-import { DataStruct } from '@/types';
-import { useCurrentUser } from '@/hooks/use-current-user';
+"use client";
+import { useEffect, useRef, useState } from "react";
+import { drawContributions } from "@/lib/graph";
+import { getUserData } from "./_getData";
+import { DataStruct } from "@/types";
+import { useCurrentUser } from "@/hooks/use-current-user";
 // import useNotes from '@/context/NotesContext';
 // import { seedUserData } from './_addData';
 // import jsonData from './mock.json';
 
 type GraphProps = {
-  isPreview: boolean
-}
+  isPreview: boolean;
+};
 
-export default function Graph (props: GraphProps) {
+export default function Graph(props: GraphProps) {
   // const {wordCount} = useNotes()
-  const isPreview = props.isPreview
+  const isPreview = props.isPreview;
   const canvasRef = useRef(null);
   const [userData, setUserData] = useState<DataStruct>();
   const currentYear = new Date().getFullYear();
-  const user = useCurrentUser()
-  const username = user?.name
-  const userId = user?.id
+  const user = useCurrentUser();
+  const username = user?.name;
+  const userId = user?.id;
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const userData = await getUserData(userId ?? ''); // Add nullish coalescing operator to provide a default value
-        setUserData(userData);
+        if (userId) {
+          const userData = await getUserData(userId); // Add nullish coalescing operator to provide a default value
+          console.log('DEUBG: /components/graph ',userData)
+          setUserData(userData);
+        }
       } catch (error) {
-        console.error('Error fetching user data:', error);
+        console.error("Error fetching user data:", error);
       }
     };
 
@@ -37,18 +40,16 @@ export default function Graph (props: GraphProps) {
 
   useEffect(() => {
     if (canvasRef.current && userData) {
-      const filteredYears: any = userData.years.filter(
-        (year) => year.year === currentYear.toString()
-      );
+      const filteredYears: any = userData.years.filter((year) => year.year === currentYear.toString());
       const filteredData = {
         years: filteredYears,
         contributions: userData.contributions,
       };
       drawContributions(canvasRef.current, {
         data: isPreview ? filteredData : userData,
-        username: username ?? '', // Provide a default value for username
-        themeName: 'solarizedDark',
-        footerText: 'Clack ©2024',
+        username: username ?? "", // Provide a default value for username
+        themeName: "solarizedDark",
+        footerText: "Clack ©2024",
         wordCount: 0, // might use later
       });
     }
@@ -59,15 +60,12 @@ export default function Graph (props: GraphProps) {
   // };
 
   return (
-    <div
-      className="border-gray-200 border-2 max-w-[325px] md:max-w-full px-1 md:p-10"
-      style={{ overflowX: 'auto' }}
-    >
+    <div className="border-gray-200 border-2 max-w-[325px] md:max-w-full px-1 md:p-10" style={{ overflowX: "auto" }}>
       {/* <button onClick={handlePostClick}>POST Data</button> */}
       <canvas className="max-w-none md:w-full h-auto" ref={canvasRef}></canvas>
     </div>
   );
-};
+}
 // import React, { useEffect, useRef } from 'react';
 // import { drawContributions } from '@/lib/graph';
 // import mockData from './mock.json';
