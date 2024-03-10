263b3481e964f8e392b12779927951e87cc6996c
phukon <hello@rikiphukon.com>
2024-03-10 15:41:25 +0530

    feat(Editor page): add dash link

--- src/app/note/page.tsx
+++ src/app/note/page.tsx
@@ -1,19 +1,25 @@
 "use client";
 
 import { notFound } from "next/navigation";
+import { IoChevronBack } from "react-icons/io5";
 import NovelEditor from "./[id]/novelEditor";
+import Link from "next/link";
 
 function Page({ searchParams }: { searchParams: { id: string } }) {
+  if (!searchParams.id) {
+    notFound();
+  }
 
-    if (!searchParams.id){
-        notFound()
-    }
-
-    return (
-        <div className="mb-12 flex min-h-[100svh] flex-col items-center sm:px-5 sm:pt-[calc(20vh)] md:mb-0">
-            <NovelEditor id={searchParams.id} />
-        </div>
-    );
+  return (
+    <div className=" flex flex-col justify-center">
+      <Link className="pt-5 flex flex-row px-5 text-gray-500 hover:underline" href="/dash">
+     <IoChevronBack  className=" w-6 h-6"/>... Dash 
+      </Link>
+      <div className="mb-12 flex min-h-[100svh] flex-col items-center sm:px-5 sm:pt-[calc(20vh)] md:mb-0">
+        <NovelEditor id={searchParams.id} />
+      </div>
+    </div>
+  );
 }
 
 export default Page;
