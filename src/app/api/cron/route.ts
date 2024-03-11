fd5be2987b5000aeb3f7548c4c3e250b260b4a50
phukon <hello@rikiphukon.com>
2024-03-11 15:50:01 +0530

    fix(CRONJOB): correct env variables

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -3,7 +3,7 @@ import { db } from '@/lib/db';
 
 export function GET(request: NextRequest) {
   const authHeader = request.headers.get('authorization');
-  if (authHeader !== `Bearer ${process.env.SECURITY_KEY}`) {
+  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
     return new Response('Unauthorized', {
       status: 401,
     });
