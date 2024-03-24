33abb75f19bc35e54cc0b2cc7a08518ef92567a7
rudrodip <official.rudrodipsarker@gmail.com>
2024-03-25 00:32:11 +0600

    removed unused div element from dom tree

--- src/components/graph/layout.tsx
+++ src/components/graph/layout.tsx
@@ -5,9 +5,9 @@ import { themes } from "@/lib/graph";
 
 const GraphLayout = ({ isPreview, themeName }: { isPreview: boolean; themeName: keyof typeof themes }) => {
   return (
-    <div>
+    <>
       <Graph isPreview={isPreview} themeName={themeName} />
-    </div>
+    </>
   );
 };
 
